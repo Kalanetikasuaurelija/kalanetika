e2336b28970b4f2bbc17e2b3e9de686312483442
Kalanetikasuaurelija <mindasveo@gmail.com>
2026-09-21 16:02:28 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Vienintelis nustatymas GitHub puslapyje. Įrašykite SAVO Worker adresą su /api.
 // Slapto API_SECRET rakto šiame faile NIEKADA nerašykite.
 window.KALANETIKA_CONFIG = {
-  apiUrl: 'https://kalanetika-api.mindasveo.workers.dev/'
+  apiUrl: 'https://kalanetika-api.mindasveo.workers.dev/api'
 };
